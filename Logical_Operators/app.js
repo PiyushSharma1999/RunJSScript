56b0ee30e46e0374041a5791e45d11e69bb9cd37
piyush sharma <piyush.sharma@mouser.com>
2024-03-11 20:31:59 +0530

    truthy falsy

---
+++ Logical_Operators/app.js
@@ -0,0 +1,15 @@
+// const password = prompt("Enter your password");
+
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid Password!")
+// } else {
+//     console.log("Invalid password. Please ensure password is longer than 6 characters and does not contain spaces.")
+// }
+
+const age = prompt("please enter your age");
+
+if (age <= 5 || age >= 65) {
+    console.log("FREE")
+} else {
+    console.log("10$")
+}
---
+++ Truthy_Falsy/app.js
@@ -0,0 +1,7 @@
+// const input = prompt("Enter Something");
+
+if (undefined) {
+    console.log("TRUTHY!")
+} else {
+    console.log("FALSY!")
+}
